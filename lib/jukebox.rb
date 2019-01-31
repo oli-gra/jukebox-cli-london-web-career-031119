ece07f8c585616c194f9342695c00fd1895b218f
Oliver <oliver.grant@gmail.com>
2019-01-31 18:31:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,6 +31,7 @@ end
 def play (songs)
 	puts "Please enter a song name or number:"
 	song_choice = gets.chomp
+	binding.pry
 	if song_choice != songs 
 		puts "Playing #{songs[song_choice-1]}"
 	else
